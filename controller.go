3581a5928ea25fb434d4f0767135720209adb850
alPrihodko <alPrihodko@gmail.com>
2016-03-08 10:00:57 +0200

    comm

--- controller.go
+++ controller.go
@@ -6,6 +6,7 @@ import (
   "net/http"
   "golang.org/x/net/websocket"
   "goHome/home"
+  "time"
 )
 
 func echoHandler(ws *websocket.Conn) {
@@ -28,11 +29,35 @@ func echoHandler(ws *websocket.Conn) {
   }
 }
 
+func schedule(what func(), delay time.Duration) chan bool {
+  stop := make(chan bool)
+
+  go func() {
+    for {
+      what()
+      select {
+        case <-time.After(delay):
+        case <-stop:
+        return
+      }
+    }
+    }()
+
+    return stop
+}
+
+
 func main() {
+
+  stop := schedule(home.ReportInternalTemp, 5*time.Second)
+
   http.Handle("/echo", websocket.Handler(echoHandler))
   http.Handle("/", http.FileServer(http.Dir(".")))
   err := http.ListenAndServe(":1234", nil)
   if err != nil {
     panic("ListenAndServe: " + err.Error())
   }
+
+  stop <- true
+
 }
