db2de11f14e916f12347b7ee2d4072f0adaf4568
alPrihodko <alPrihodko@gmail.com>
2016-03-13 20:50:51 +0200

    report temp to cloud

--- controller.go
+++ controller.go
@@ -1,76 +1,80 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net/http"
-  "golang.org/x/net/websocket"
-  "goHome/home"
-  "time"
+	"fmt"
+	"goHome/home"
+	"log"
+	"net/http"
+	"time"
+
+	"golang.org/x/net/websocket"
 )
 
 func reportInternalSensor(s *home.Sensors) {
-  v, err := s.InternalSensor();
-  if (err != nil) {
-    panic("ReadSensor: " + err.Error())
-  }
-  log.Println("\t", v);
+	v, err := s.InternalSensor()
+	if err != nil {
+		home.ReportAlert(err.Error(), "Cannot get internal Temp to Phant")
+	}
+	log.Println("\t", v)
+	err = home.ReportInternalTemp(v)
+	if err != nil {
+		home.ReportAlert(err.Error(), "Cannot report internal Temp to Phant")
+	}
 }
 
 func echoHandler(ws *websocket.Conn) {
-  msg := make([]byte, 512)
-  for {
-    n, err := ws.Read(msg)
-    if err != nil {
-      log.Println(err)
-      return
-    }
+	msg := make([]byte, 512)
+	for {
+		n, err := ws.Read(msg)
+		if err != nil {
+			log.Println(err)
+			return
+		}
 
-    fmt.Printf("Receive: %s\n", msg[:n])
-    home.ToggleHeatMotor1()
-    m, err := ws.Write(msg[:n])
-    if err != nil {
-      log.Println(err)
-      return
-    }
-    fmt.Printf("Send: %s\n", msg[:m])
-  }
+		fmt.Printf("Receive: %s\n", msg[:n])
+		home.ToggleHeatMotor1()
+		m, err := ws.Write(msg[:n])
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Printf("Send: %s\n", msg[:m])
+	}
 }
 
 func schedule(what func(*home.Sensors), delay time.Duration, s *home.Sensors) chan bool {
-  stop := make(chan bool)
+	stop := make(chan bool)
 
-  go func() {
-    for {
-      what(s)
-      select {
-        case <-time.After(delay):
-        case <-stop:
-        return
-      }
-    }
-    }()
+	go func() {
+		for {
+			what(s)
+			select {
+			case <-time.After(delay):
+			case <-stop:
+				return
+			}
+		}
+	}()
 
-    return stop
+	return stop
 }
-
 
 func main() {
 
-  sensors, err := home.NewSensors()
-  if err != nil {
-    panic("Sensors: " + err.Error())
-  }
+	sensors, err := home.NewSensors()
+	if err != nil {
+		panic("Sensors: " + err.Error())
+	}
 
-  stop := schedule(reportInternalSensor, 600*time.Second, sensors)
+	stop := schedule(reportInternalSensor, 60*time.Second, sensors)
 
-  http.Handle("/echo", websocket.Handler(echoHandler))
-  http.Handle("/", http.FileServer(http.Dir(".")))
-  err = http.ListenAndServe(":1234", nil)
-  if err != nil {
-    panic("ListenAndServe: " + err.Error())
-  }
+	http.Handle("/echo", websocket.Handler(echoHandler))
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	err = http.ListenAndServe(":1234", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
 
-  stop <- true
+	stop <- true
 
 }
