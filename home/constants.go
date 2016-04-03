6cda980f7e6f17b6773dcdcd7d23d0cf4dc2906e
alPrihodko <alPrihodko@gmail.com>
2016-04-03 22:51:38 +0300

    constants fixed

--- home/constants.go
+++ home/constants.go
@@ -10,7 +10,7 @@ const (
 	phantPrivateTemp = "jkybBZw5j9F0xxmwWar1"
 	phantPublicTemp  = "rozL4pN5jMC466MKl8AE"
 
-	iotURL                  = "http://myhome.iot.touchpanelcontrol.com/v1/"
+	iotURL                  = "http://myhome.iot.touchpanelcontrol.com/v1"
 	iotCloudAPIKey          = "35d1d4d0-f80f-11e5-83f9-7d8483cd58bb"
 	iotCloudHomeDevice      = "pi"
 	iotCloudInternalTempVar = "tempInternal"
