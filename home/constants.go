d15f6a246a54619c5ccfbccf0f876f3cc0a35ab6
alPrihodko <alPrihodko@gmail.com>
2016-04-03 22:43:10 +0300

    added iot

--- home/constants.go
+++ home/constants.go
@@ -5,7 +5,13 @@ const (
 	emailTo           = "alPrihodko@gmail.com"
 	googleAccountName = "alPrihodko@gmail.com"
 	googleAPIKey      = "ahvpiuembqkvszpi"
-	cloudURL          = "http://data.sparkfun.com/input/"
-	phantPrivateTemp  = "jkybBZw5j9F0xxmwWar1"
-	phantPublicTemp   = "rozL4pN5jMC466MKl8AE"
+
+	cloudURL         = "http://data.sparkfun.com/input/"
+	phantPrivateTemp = "jkybBZw5j9F0xxmwWar1"
+	phantPublicTemp  = "rozL4pN5jMC466MKl8AE"
+
+	iotURL                  = "http://myhome.iot.touchpanelcontrol.com/v1/"
+	iotCloudAPIKey          = "35d1d4d0-f80f-11e5-83f9-7d8483cd58bb"
+	iotCloudHomeDevice      = "pi"
+	iotCloudInternalTempVar = "tempInternal"
 )
