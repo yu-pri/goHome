9b89352755c6167772ca29a5e90355015b227382
alPrihodko <alPrihodko@gmail.com>
2016-05-11 09:27:10 +0300

    work with relays

--- v/js/app.js
+++ v/js/app.js
@@ -39,29 +39,20 @@ var Temperature = React.createClass({
     this.setState({
       temp4: data.TempHeater
     });
+
+    this.setState({
+      heater: data.HeaterState
+    });
+
+    this.setState({
+      heaterPump: data.PumpState
+    });
+
   },
 
   componentWillMount() {
     window.N.TempDataHandler.callback = (data) => {
       this.set(data);
-      /*
-      this.setState({
-        temp1: data.TempInside
-      });
-
-      this.setState({
-        temp2: data.TempReverse
-      });
-
-      this.setState({
-        temp3: data.TempEntryRoom
-      });
-
-
-      this.setState({
-        temp4: data.TempHeater
-      });
-      */
     };
   },
 
@@ -71,19 +62,13 @@ var Temperature = React.createClass({
       <div>
         <div className="mui-container-fluid" >
           <span className="temp">Kitchen:</span> {this.state.temp1}
-        </div>
-
-        <div className="mui-container-fluid" >
           <span className="temp">Entry room:</span> {this.state.temp3}
-        </div>
-
-        <div className="mui-container-fluid" >
           <span className="temp">H Rev: </span>{this.state.temp2}
         </div>
 
-
         <div className="mui-container-fluid" >
           <span className="temp">Heater: </span> {this.state.temp4}
+          , Heater: {this.state.heater}, HeaterPump: {this.state.heaterPump}
         </div>
 
 
