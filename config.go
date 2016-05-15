185dba1f05039fbc4202c0ce2ab747e776b8fdaf
alPrihodko <alPrihodko@gmail.com>
2016-05-15 22:53:18 +0300

    save config

---
+++ config.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"goHome/home"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+type config struct {
+	PumpState   string `json:"PumpState, string"`
+	HeaterState string `json:"HeaterState, string"`
+}
+
+/*Config contains user preferences*/
+type Config config
+
+/*
+func init() {
+	log.Println("Loading user preferences")
+	Config.HeaterState = home.AUTO
+	Config.PumpState = home.AUTO
+	//runtime.se
+}
+*/
+
+func (q *Config) saveConfig() error {
+	b, err := json.Marshal(q)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	err = ioutil.WriteFile(configFileName, b, 0644)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func (q *Config) setDefault() {
+	q.HeaterState = home.AUTO
+	q.PumpState = home.AUTO
+}
+
+func (q *Config) loadConfig() error {
+
+	configFile, err := os.Open(configFileName)
+	if err != nil {
+		log.Println("Cannot open config: ", err.Error())
+		q.setDefault()
+		return err
+	}
+
+	jsonParser := json.NewDecoder(configFile)
+	if err = jsonParser.Decode(q); err != nil {
+		log.Println("Error parsing config: ", err.Error())
+		q.setDefault()
+		return err
+	}
+
+	return nil
+}
