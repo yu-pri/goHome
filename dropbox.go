0132949edfd0974fe6bf1f13968b1e88eefb74b9
alPrihodko <alPrihodko@gmail.com>
2016-05-18 01:06:52 +0300

    dropbox

--- dropbox.go
+++ dropbox.go
@@ -49,7 +49,7 @@ func scheduleBackup(what func(*home.HistoryData, string), delay time.Duration,
 func backupHistoryData(q *home.HistoryData, local string) {
 	historyData.SerializeToFile(local)
 
-	if _, err := DB.UploadFile(local, "/backup", true, "main"); err != nil {
+	if _, err := DB.UploadFile(local, "/backup", true, "1"); err != nil {
 		log.Printf("Error uploading %s: %s\n", local, err)
 	} else {
 		log.Printf("File %s successfully uploaded\n", local)
