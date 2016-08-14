51ed18bcd8c8f42a48fedce82baa02675c778f93
alPrihodko <alPrihodko@gmail.com>
2016-08-14 18:18:08 +0300

    disable reporting to cloud

--- controller.go
+++ controller.go
@@ -136,43 +136,52 @@ func main() {
 
 		})
 
-		gobot.On(home.SmokeAlarmKitchen.Event("release"), func(data interface{}) {
-			log.Println("Smoke alarm in kitchen: On")
-			err = home.Sms("Test", "Smoke/Gas detected in Kitchen", home.Recipients)
+	}
+
+	gobot.On(home.SmokeAlarmKitchen.Event("release"), func(data interface{}) {
+		log.Println("Smoke alarm in the kitchen: On")
+		/*
+			err = home.Sms("Test", "Smoke/Gas detected in the Kitchen", home.Recipients)
 			if err != nil {
 				log.Println(err.Error())
 			}
 			home.ReportAlert("Something is really not OK", "Smoke/Gas detected in the Kitchen")
-		})
+		*/
+	})
 
-		gobot.On(home.SmokeAlarmKitchen.Event("push"), func(data interface{}) {
-			log.Println("Smoke alarm in kitchen: Off")
+	gobot.On(home.SmokeAlarmKitchen.Event("push"), func(data interface{}) {
+		log.Println("Smoke alarm in the kitchen: Off")
+		/*
 			err = home.Sms("Test", "Smoke/Gas in the Kitchen - all good", home.Recipients)
 			if err != nil {
 				log.Println(err.Error())
 			}
 			home.ReportAlert("Now it's better", "Smoke/Gas in the Kitchen - all good")
-		})
+		*/
+	})
 
-		gobot.On(home.SmokeAlarmSauna.Event("push"), func(data interface{}) {
-			log.Println("Smoke alarm in sauna: On")
+	gobot.On(home.SmokeAlarmSauna.Event("release"), func(data interface{}) {
+		log.Println("Smoke alarm in the Sauna: On")
+		/*
 			err = home.Sms("Test", "Smoke/Gas detected in the Sauna", home.Recipients)
 			if err != nil {
 				log.Println(err.Error())
 			}
-			home.ReportAlert("Something is really not OK", "Smoke/Gas detected in Sauna")
-		})
+			home.ReportAlert("Something is really not OK", "Smoke/Gas detected in the Sauna")
+		*/
+	})
 
-		gobot.On(home.SmokeAlarmSauna.Event("release"), func(data interface{}) {
-			log.Println("Smoke alarm in sauna: Off")
+	gobot.On(home.SmokeAlarmSauna.Event("push"), func(data interface{}) {
+		log.Println("Smoke alarm in the Sauna: Off")
+
+		/*
 			err = home.Sms("Test", "Smoke/Gas in Sauna - all good", home.Recipients)
 			if err != nil {
 				log.Println(err.Error())
 			}
 			home.ReportAlert("Now it's better", "Smoke/Gas in Sauna - all good")
-		})
-
-	}
+		*/
+	})
 
 	//TODO: more relays here
 	//home.GetHeat2(),
