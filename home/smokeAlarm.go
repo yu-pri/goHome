b49004f8c2a3d41fd634ffd3f14b0d5f1a8306d7
alPrihodko <alPrihodko@gmail.com>
2016-10-09 06:43:32 +0300

    gas alarm improved

--- home/smokeAlarm.go
+++ home/smokeAlarm.go
@@ -13,6 +13,9 @@ var firstTimeSauna = true
 var alarmTimeSauna int32
 var alarmTimeKitchen int32
 
+var saunaAlarmStatus = false
+var kitchenAlarmStatus = false
+
 /*
 SmokeAlarmKitchen smoke/gas sensor in the kitchen
 */
@@ -25,13 +28,15 @@ var SmokeAlarmSauna = gpio.NewButtonDriver(r, "alm2", "18") //24
 
 func init() {
 	log.Println("Initialise smoke alarms")
+
 	gobot.On(SmokeAlarmKitchen.Event("release"), func(data interface{}) {
 		now := int32(time.Now().Unix())
 		//log.Println(now - alarmTimeKitchen)
-		if (now - alarmTimeKitchen) < 2 {
+		if (now-alarmTimeKitchen) < 2 || kitchenAlarmStatus == true {
 			return
 		}
 		alarmTimeKitchen = now
+		kitchenAlarmStatus = true
 
 		log.Println("Smoke alarm in the kitchen: On")
 
@@ -53,10 +58,11 @@ func init() {
 		}
 
 		now := int32(time.Now().Unix())
-		if (now - alarmTimeKitchen) < 2 {
+		if (now-alarmTimeKitchen) < 2 || kitchenAlarmStatus == false {
 			return
 		}
 		alarmTimeKitchen = now
+		kitchenAlarmStatus = false
 
 		log.Println("Smoke alarm in the kitchen: Off")
 
@@ -74,10 +80,12 @@ func init() {
 	gobot.On(SmokeAlarmSauna.Event("release"), func(data interface{}) {
 		now := int32(time.Now().Unix())
 		//log.Println(now - alarmTimeKitchen)
-		if (now - alarmTimeSauna) < 2 {
+		if now-alarmTimeSauna < 2 || saunaAlarmStatus == true {
 			return
 		}
+
 		alarmTimeSauna = now
+		saunaAlarmStatus = true
 
 		log.Println("Smoke alarm in the Sauna: On")
 		/*
@@ -99,10 +107,12 @@ func init() {
 
 		now := int32(time.Now().Unix())
 		//log.Println(now - alarmTimeKitchen)
-		if (now - alarmTimeSauna) < 2 {
+		if now-alarmTimeSauna < 2 || saunaAlarmStatus == false {
 			return
 		}
+
 		alarmTimeSauna = now
+		saunaAlarmStatus = false
 
 		log.Println("Smoke alarm in the Sauna: Off")
 		/*
