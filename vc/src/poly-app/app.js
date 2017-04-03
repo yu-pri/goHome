bed9248751df77df5247f994a25b544d113588be
al <alprihodko@gmail.com>
2017-04-03 14:22:52 +0300

    adding new ui

---
+++ vc/src/poly-app/app.js
@@ -0,0 +1,12 @@
+class StaticGlobals {
+  //const HOST = "sasha123.ddns.ukrtel.net:1234";
+
+  constructor() {
+    console.log("constants initiates");
+  };
+
+  static GetHostName() {
+    return "sasha123.ddns.ukrtel.net:1234";
+  };
+
+};
